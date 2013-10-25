a3becbdff53c2211cf264e6a0b32b57b966bf1c2
Matt Parker <mdparker89@gmail.com>
2013-10-24 21:18:05 -0400

    improve test coverage of models

--- models/User.go
+++ models/User.go
@@ -4,42 +4,58 @@ import (
 	"code.google.com/p/go.crypto/bcrypt"
 	"encoding/json"
 	"fmt"
+	"github.com/nu7hatch/gouuid"
 	"github.com/streadway/amqp"
 	rbtmq "github.com/wurkhappy/Rabbitmq-go-wrapper"
 	"github.com/wurkhappy/WH-UserService/DB"
 	"labix.org/v2/mgo/bson"
-	"log"
+	// "log"
 	"net/http"
 	"time"
 )
 
 type User struct {
-	ID          bson.ObjectId `json:"id" bson:"_id"`
-	FirstName   string        `json:"firstName"`
-	LastName    string        `json:"lastName"`
-	Email       string        `json:"email"`
-	PwHash      []byte        `json:"-"`
-	AvatarURL   string        `json:"avatarURL"`
-	PhoneNumber string        `json:"phoneNumber"`
-	DateCreated time.Time     `json:"dateCreated"`
-	IsVerified  bool          `json:"isVerified"`
+	ID          string    `json:"id" bson:"_id"`
+	FirstName   string    `json:"firstName"`
+	LastName    string    `json:"lastName"`
+	Email       string    `json:"email"`
+	PwHash      []byte    `json:"-"`
+	AvatarURL   string    `json:"avatarURL"`
+	PhoneNumber string    `json:"phoneNumber"`
+	DateCreated time.Time `json:"dateCreated"`
+	IsVerified  bool      `json:"isVerified"`
+}
+
+var connection *amqp.Connection
+var emailExchange string = "email"
+var emailQueue string = "email"
+
+func init() {
+	var err error
+	uri := "amqp://guest:guest@localhost:5672/"
+	connection, err = amqp.Dial(uri)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func NewUser() *User {
+	id, _ := uuid.NewV4()
 	return &User{
 		DateCreated: time.Now(),
-		ID:          bson.NewObjectId(),
+		ID:          id.String(),
 	}
 }
 
 //SetPassword takes a plaintext password and hashes it with bcrypt and sets the
 //password field to the hash.
-func (u *User) SetPassword(password string) {
+func (u *User) SetPassword(password string) error {
 	hpass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	u.PwHash = hpass
+	return nil
 }
 
 func (u *User) SaveUserWithCtx(ctx *DB.Context) (err error) {
@@ -59,7 +75,7 @@ func FindUserByEmail(email string, ctx *DB.Context) (u *User, err error) {
 }
 
 func FindUserByID(id string, ctx *DB.Context) (u *User, err error) {
-	err = ctx.Database.C("users").Find(bson.M{"_id": bson.ObjectIdHex(id)}).One(&u)
+	err = ctx.Database.C("users").Find(bson.M{"_id": id}).One(&u)
 	if err != nil {
 		return nil, err
 	}
@@ -67,18 +83,17 @@ func FindUserByID(id string, ctx *DB.Context) (u *User, err error) {
 }
 
 func DeleteUserWithID(id string, ctx *DB.Context) (err error) {
-	err = ctx.Database.C("users").RemoveId(bson.ObjectIdHex(id))
+	err = ctx.Database.C("users").RemoveId(id)
 	if err != nil {
-		log.Print(err)
 		return err
 	}
 	return nil
 }
 
 func FindUsers(ids []string, ctx *DB.Context) []*User {
-	adjustedIDs := make([]bson.ObjectId, 0, len(ids))
+	adjustedIDs := make([]string, 0, len(ids))
 	for _, id := range ids {
-		adjustedIDs = append(adjustedIDs, bson.ObjectIdHex(id))
+		adjustedIDs = append(adjustedIDs, id)
 	}
 	var users []*User
 	ctx.Database.C("users").Find(bson.M{"_id": bson.M{"$in": adjustedIDs}}).All(&users)
@@ -96,7 +111,7 @@ func (u *User) PasswordIsValid(password string) bool {
 
 func (u *User) AddToPaymentProcessor() {
 	client := &http.Client{}
-	r, _ := http.NewRequest("POST", "http://localhost:3120/user/"+u.ID.Hex(), nil)
+	r, _ := http.NewRequest("POST", "http://localhost:3120/user/"+u.ID, nil)
 	_, err := client.Do(r)
 	if err != nil {
 	}
@@ -106,34 +121,27 @@ func (u *User) SendVerificationEmail() {
 	message := map[string]interface{}{
 		"Body": u,
 	}
-	uri := "amqp://guest:guest@localhost:5672/"
-	connection, err := amqp.Dial(uri)
-	if err != nil {
-		panic(err)
-	}
-	defer connection.Close()
-
 	body, _ := json.Marshal(message)
-	publisher, _ := rbtmq.NewPublisher(connection, "email", "direct", "email", "/user/verify")
-	publisher.Publish(body, true)
+	sendEmail("/user/verify", body)
 }
 
 func (u *User) SendForgotPasswordEmail() {
 	message := map[string]interface{}{
 		"Body": map[string]interface{}{
-			"user":     u,
+			"user": u,
 		},
 	}
-	uri := "amqp://guest:guest@localhost:5672/"
-	connection, err := amqp.Dial(uri)
-	if err != nil {
-		panic(err)
-	}
-	defer connection.Close()
-
 	body, _ := json.Marshal(message)
-	publisher, _ := rbtmq.NewPublisher(connection, "email", "direct", "email", "/user/password/forgot")
+	sendEmail("/user/password/forgot", body)
+}
+
+func sendEmail(path string, body []byte) error {
+	publisher, err := rbtmq.NewPublisher(connection, emailExchange, "direct", emailQueue, path)
+	if err != nil {
+		return err
+	}
 	publisher.Publish(body, true)
+	return nil
 }
 
 func (u *User) ValidateNewPassword(pw string) error {
--- models/user_test.go
+++ models/user_test.go
@@ -1,15 +1,23 @@
 package models
 
 import (
+	"encoding/json"
 	"github.com/wurkhappy/WH-UserService/DB"
 	"labix.org/v2/mgo"
 	"testing"
 )
 
+func init() {
+	emailExchange = "test"
+	emailQueue = "test"
+}
+
 func TestUnitTests(t *testing.T) {
 	testNewUser(t)
 	testSetPassword(t)
 	testPasswordIsValid(t)
+	testValidateNewPassword(t)
+	testIsUserRegistered(t)
 }
 
 func TestIntegrationTests(t *testing.T) {
@@ -25,7 +33,9 @@ func TestIntegrationTests(t *testing.T) {
 		testFindUserByEmail(t, ctx)
 		testFindUserByID(t, ctx)
 		testDeleteUser(t, ctx)
-
+		testFindUsers(t, ctx)
+		testSyncWithExistingInvitation(t, ctx)
+		testsendEmail(t)
 	}
 }
 
@@ -34,11 +44,17 @@ func ClearDB(ctx *DB.Context) {
 	ctx.Database.C("users").DropCollection()
 }
 
+func testInit(t *testing.T) {
+	if connection == nil {
+		t.Errorf("%s--- rabbitmq connection wasn't initialized", "testInit")
+	}
+}
+
 //tests
 func testNewUser(t *testing.T) {
 	user := NewUser()
 
-	if !user.ID.Valid() {
+	if user.ID == "" {
 		t.Errorf("%s--- user id not valid", "testNewUser")
 	}
 }
@@ -50,6 +66,11 @@ func testSetPassword(t *testing.T) {
 
 	if output, input := user.PwHash, password; string(output) == input {
 		t.Errorf("%s--- input:%s, output: %s", "testSetPassword", input, output)
+	}
+
+	err := user.SetPassword("")
+	if err == nil {
+		t.Errorf("%s--- does not return error on invalid input", "testSetPassword")
 	}
 }
 
@@ -78,15 +99,20 @@ func testFindUserByEmail(t *testing.T, ctx *DB.Context) {
 	if u == nil {
 		t.Errorf("%s--- user was not found", "testFindUserByEmail")
 	}
+
+	_, err = FindUserByEmail("badrequest@bad.com", ctx)
+	if err == nil {
+		t.Errorf("%s--- DB returned a bad request", "testFindUserByEmail")
+	}
 }
 
 func testFindUserByID(t *testing.T, ctx *DB.Context) {
 	user := NewUser()
 	user.SaveUserWithCtx(ctx)
 
-	u, _ := FindUserByID(user.ID.Hex(), ctx)
+	u, _ := FindUserByID(user.ID, ctx)
 
-	if len(u.ID.Hex()) == 0 {
+	if len(u.ID) == 0 {
 		t.Errorf("%s--- user was not found", "testFindUserByID")
 	}
 }
@@ -94,15 +120,33 @@ func testFindUserByID(t *testing.T, ctx *DB.Context) {
 func testDeleteUser(t *testing.T, ctx *DB.Context) {
 	user := NewUser()
 	user.SaveUserWithCtx(ctx)
-	err := DeleteUserWithID(user.ID.Hex(), ctx)
+	err := DeleteUserWithID(user.ID, ctx)
 
 	if err != nil {
 		t.Errorf("%s--- error is:%v", "testDeleteUser", err)
 	}
 
-	u, err := FindUserByID(user.ID.Hex(), ctx)
+	u, err := FindUserByID(user.ID, ctx)
 	if u != nil {
 		t.Errorf("%s--- user was found", "testDeleteUser")
+	}
+
+	err = DeleteUserWithID("invalid-id", ctx)
+	if err == nil {
+		t.Errorf("%s--- DB deleted with invalid id", "testDeleteUser")
+	}
+}
+
+func testFindUsers(t *testing.T, ctx *DB.Context) {
+	user1 := NewUser()
+	user2 := NewUser()
+
+	user1.SaveUserWithCtx(ctx)
+	user2.SaveUserWithCtx(ctx)
+
+	users := FindUsers([]string{user1.ID, user2.ID}, ctx)
+	if len(users) != 2 {
+		t.Errorf("%s--- all users were not found", "testFindUsers")
 	}
 }
 
@@ -111,9 +155,77 @@ func testPasswordIsValid(t *testing.T) {
 	password := "password"
 	user.SetPassword(password)
 
-	user.PasswordIsValid(password)
-
 	if !user.PasswordIsValid(password) {
 		t.Errorf("%s--- invalid password", "testPasswordIsValid")
 	}
+
+	if user.PasswordIsValid("invalid") {
+		t.Errorf("%s--- invalid password passed", "testPasswordIsValid")
+	}
+}
+
+func testValidateNewPassword(t *testing.T) {
+	user := new(User)
+	err := user.ValidateNewPassword("123456")
+	if err != nil {
+		t.Errorf("%s--- valid password failing", "testValidateNewPassword")
+	}
+
+	err = user.ValidateNewPassword("12345")
+	if err == nil {
+		t.Errorf("%s--- invalid password validating", "testValidateNewPassword")
+	}
+}
+
+func testIsUserRegistered(t *testing.T) {
+	user := new(User)
+	if user.IsUserRegistered() {
+		t.Errorf("%s--- unregistered user is returning as registered", "testIsUserRegistered")
+	}
+	user.PwHash = []byte("some-string")
+	if !user.IsUserRegistered() {
+		t.Errorf("%s--- registered user is returning as unregistered", "testIsUserRegistered")
+	}
+}
+
+func testSyncWithExistingInvitation(t *testing.T, ctx *DB.Context) {
+	user := NewUser()
+	user.Email = "testSyncWithExistingInvitation@wh.com"
+	user.SaveUserWithCtx(ctx)
+
+	user2 := new(User)
+	user2.Email = user.Email
+	err := user2.SyncWithExistingInvitation(ctx)
+	if err != nil {
+		t.Errorf("%s--- didn't sync user", "testSyncWithExistingInvitation")
+	}
+	if user2.ID != user.ID {
+		t.Errorf("%s--- didn't sync ID", "testSyncWithExistingInvitation")
+	}
+
+	user.SetPassword("password")
+	user.SaveUserWithCtx(ctx)
+
+	user3 := new(User)
+	user3.Email = user.Email
+	err = user3.SyncWithExistingInvitation(ctx)
+	if err == nil {
+		t.Errorf("%s--- failed to reject registered user", "testSyncWithExistingInvitation")
+	}
+}
+
+func testsendEmail(t *testing.T) {
+	path := "test"
+	u := NewUser()
+	message := map[string]interface{}{
+		"Body": map[string]interface{}{
+			"user": u,
+		},
+	}
+	body, _ := json.Marshal(message)
+	err := sendEmail(path, body)
+	if err != nil {
+		t.Errorf("%s--- error sending email", "testsendEmail")
+	}
+
 }
