6c5f65dd7b013dc987fbfc5b58a5e797c3bd0853
Matt Parker <mdparker89@gmail.com>
2013-11-12 10:33:05 -0500

    finish zeromq switch

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"bytes"
+	// "bytes"
 	"encoding/json"
 	"github.com/wurkhappy/WH-Config"
 	"github.com/wurkhappy/WH-UserService/models"
 	"github.com/wurkhappy/mdp"
-	"net/http"
+	// "net/http"
 	"net/url"
-	"strconv"
+	// "strconv"
+	// "log"
 )
 
 type ServiceReq struct {
@@ -30,37 +31,39 @@ func main() {
 		go route(worker)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		//route to function based on the path and method
-		route, pathParams, _ := router.FindRoute(r.URL.String())
-		routeMap := route.Dest.(map[string]interface{})
-		handler := routeMap[r.Method].(func(map[string]interface{}, []byte) ([]byte, error, int))
+	select {}
 
-		//parse the request
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(r.Body)
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	//route to function based on the path and method
+	// 	route, pathParams, _ := router.FindRoute(r.URL.String())
+	// 	routeMap := route.Dest.(map[string]interface{})
+	// 	handler := routeMap[r.Method].(func(map[string]interface{}, []byte) ([]byte, error, int))
 
-		//add url params to params var
-		params := make(map[string]interface{})
-		for key, value := range pathParams {
-			params[key] = value
-		}
-		//add url query params
-		values := r.URL.Query()
-		for key, value := range values {
-			params[key] = value
-		}
+	// 	//parse the request
+	// 	buf := new(bytes.Buffer)
+	// 	buf.ReadFrom(r.Body)
 
-		//run handler and do standard http stuff(write JSON, return err, set status code)
-		jsonData, err, statusCode := handler(params, buf.Bytes())
-		if err != nil {
-			http.Error(w, `{"status_code":`+strconv.Itoa(statusCode)+`, "description":"`+err.Error()+`"}`, statusCode)
-			return
-		}
-		w.WriteHeader(statusCode)
-		w.Write(jsonData)
-	})
-	http.ListenAndServe(":3000", nil)
+	// 	//add url params to params var
+	// 	params := make(map[string]interface{})
+	// 	for key, value := range pathParams {
+	// 		params[key] = value
+	// 	}
+	// 	//add url query params
+	// 	values := r.URL.Query()
+	// 	for key, value := range values {
+	// 		params[key] = value
+	// 	}
+
+	// 	//run handler and do standard http stuff(write JSON, return err, set status code)
+	// 	jsonData, err, statusCode := handler(params, buf.Bytes())
+	// 	if err != nil {
+	// 		http.Error(w, `{"status_code":`+strconv.Itoa(statusCode)+`, "description":"`+err.Error()+`"}`, statusCode)
+	// 		return
+	// 	}
+	// 	w.WriteHeader(statusCode)
+	// 	w.Write(jsonData)
+	// })
+	// http.ListenAndServe(":3000", nil)
 }
 
 type Resp struct {
