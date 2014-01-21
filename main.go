caa76c87688e5edb882f3006626212a38023407c
Matt Parker <mdparker89@gmail.com>
2014-01-21 17:29:07 -0500

    fix worker registered name

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func main() {
 	var wg sync.WaitGroup
 
 	for i := 0; i < gophers; i++ {
-		worker := mdp.NewWorker(config.MDPBroker, config.AgreementsService, false)
+		worker := mdp.NewWorker(config.MDPBroker, config.UserService, false)
 		defer worker.Close()
 		go route(worker, shutChan, wg)
 	}
