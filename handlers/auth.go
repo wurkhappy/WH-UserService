c50c1b326ba361343babfb8ef7916db1c0b0a958
MattParker89 <mdparker89@gmail.com>
2014-06-09 10:32:09 -0400

    fix lower casing email for login

--- handlers/auth.go
+++ handlers/auth.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/wurkhappy/WH-UserService/models"
 	"net/http"
+	"strings"
 	// "log"
 )
 
@@ -13,16 +14,24 @@ func Login(params map[string]interface{}, body []byte) ([]byte, error, int) {
 	json.Unmarshal(body, &requestData)
 	fmt.Println(requestData["email"])
 
-	user, err := models.FindUserByEmail(requestData["email"].(string))
+	email, ok := requestData["email"].(string)
+
+	if !ok {
+		return nil, fmt.Errorf("Please include an email address"), http.StatusBadRequest
+	}
+
+	user, err := models.FindUserByEmail(strings.ToLower(email))
 	if user == nil || err != nil {
 		return nil, fmt.Errorf("Sorry, we couldn't find an account with that email and password combination"), http.StatusBadRequest
 	}
 
-	if _, ok := requestData["password"]; !ok {
+	password, ok := requestData["password"].(string)
+
+	if !ok {
 		return nil, fmt.Errorf("Oops! Looks like you forget to enter your password."), http.StatusBadRequest
 	}
 
-	if !user.PasswordIsValid(requestData["password"].(string)) {
+	if !user.PasswordIsValid(password) {
 		return nil, fmt.Errorf("Sorry, we couldn't find an account with that email and password combination"), http.StatusBadRequest
 	}
 
